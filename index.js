b86dd7c221308f89d8ecfd8e9fd0712cef55841d
yuenthon191 <62110706+yuenthon191@users.noreply.github.com>
2020-08-05 14:39:51 +0700

    Update index.js

--- index.js
+++ index.js
@@ -75,7 +75,7 @@ console.log('-----ปริ้นทั้งหมด-----' + JSON.stringify(da
 function data_enter(sende_r, beacon_id) {
 
 
-   var checktime = "https://script.google.com/macros/s/AKfycbxxDwUO-Z2ch6UYBdqS0sj_N7HFFkGqJa2Ab6Hk/exec?userId="+sende_r+"&beaconid="+beacon_id;
+   var checktime = "https://script.google.com/macros/s/AKfycbymuvPlVpkzE2bAqxuWNSZAhxM-4jpk4SWxSXA/exec?userId="+sende_r+"&beaconid="+beacon_id;
  request(checktime, function (error, response, body) {
 
 })
@@ -108,32 +108,32 @@ var options = {
         var info = JSON.parse(body);
         name = info.displayName;
         
-          if(beacon_id == "013dfbdd50"){
+          if(beacon_id == "0140da277c"){
          var msg = {
            "type": "text",
-           "text": "ระบบได้บันทึกการตรวจจุดตรวจ Alk. scrubber ของ"+" " +name +" " + "ไว้เรียบร้อยแล้ว"
+           "text": "ระบบได้บันทึกการตรวจจุดตรวจอาคารขนมไทย ของ"+" " +name +" " + "ไว้เรียบร้อยแล้ว"
 
           }; 
         reply(reToken, msg, sende_r, beacon_id);    
-         }else if(beacon_id == "013e02417c"){
+         }else if(beacon_id == "0140daf42c"){
          var msg = {
            "type": "text",
-           "text": "ระบบได้บันทึกการตรวจจุดตรวจด้านหลังโรงงานของ"+" " +name +" " + "ไว้เรียบร้อยแล้ว"
+           "text": "ระบบได้บันทึกการตรวจจุดตรวจโรงเก็บขยะรีไซเคิล ของ"+" " +name +" " + "ไว้เรียบร้อยแล้ว"
           }; 
         reply(reToken, msg, sende_r, beacon_id);   
          }
          
-        else if(beacon_id == "013d89cf47"){
+        else if(beacon_id == "0140dc638b"){
          var msg = {
            "type": "text",
-           "text": "ระบบได้บันทึกการตรวจจุดตรวจคลับเฮ้าสของ"+" " +name +" " + "ไว้เรียบร้อยแล้ว"
+           "text": "ระบบได้บันทึกการตรวจจุดตรวจอาคารเบเกอรี์ ของ"+" " +name +" " + "ไว้เรียบร้อยแล้ว"
           }; 
         reply(reToken, msg, sende_r, beacon_id);   
          }
-        else if(beacon_id == "013b4dc240"){
+        else if(beacon_id == "0140dc715b"){
          var msg = {
            "type": "text",
-           "text": "ระบบได้บันทึกการตรวจจุดตรวจจุดตกแต่งของ"+" " +name +" " + "ไว้เรียบร้อยแล้ว"
+           "text": "ระบบได้บันทึกการตรวจจุดตรวจอาคารไส้ผลิตภัณฑ์ ของ"+" " +name +" " + "ไว้เรียบร้อยแล้ว"
           }; 
         reply(reToken, msg, sende_r, beacon_id);   
          }
